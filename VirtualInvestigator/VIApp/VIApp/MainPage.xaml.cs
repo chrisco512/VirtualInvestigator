e0c12b7e9627405f804660944aa7246f36b0d6c6
Kevin Dahdah <orc0909@gmail.com>
2013-04-12 01:21:36 -0400

    Added phone main page, probably doesn't work.

---
+++ VirtualInvestigator/VIApp/VIApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace VIApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void gameButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
+        }
+
+        private void leaderboardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Simple button Click event handler to take us to the second page
+    }
+}
